fdb53ea90555ea8233b7466d0af6cca856307392
Florian Sabonchi <54689374+florian-sabonchi@users.noreply.github.com>
2023-12-11 20:59:54 +0100

    fix: task-1

--- übungsblatt_8.rkt
+++ übungsblatt_8.rkt
@@ -2,22 +2,20 @@
 
 ; Aufgabe 1
 
-(define (count-item char liste counter)
-  (if (pair? liste)
-      (count-item char (cdr liste)
-                  (if (eq? (car liste) char) (+ counter 1) counter))  counter))
-
+(define (zaehle-symbol liste symbol zaehler)
+  (if (and (pair? liste) (eq? (car liste) symbol))
+      (zaehle-symbol (cdr liste) symbol (+ zaehler 1))
+      (list zaehler liste)))
 
 
 (define (compress liste)
-  (define (compress-helper liste result)    
-      (if (pair? liste)
-          (let ((c_char (car liste)))
-            (compress-helper (cdr liste)
-                             (if (memq c_char result)
-                                 result
-                                 (append result (list c_char) (list (count-item c_char liste 0)))))) result))
-  (compress-helper liste (list )))
+  (define (compress-iter c_liste ergebniss)
+    (let* ((symbol (if (pair? c_liste) (car c_liste) null)) (zaehle-ergebniss (zaehle-symbol c_liste symbol 0)))
+      (if (pair? c_liste)
+          (compress-iter (flatten (cdr zaehle-ergebniss))
+                         (append ergebniss (if (> (car zaehle-ergebniss) 1)
+                                               (list (car zaehle-ergebniss) (car c_liste)) (list (car c_liste))))) ergebniss)))
+  (compress-iter liste null))
 
 
 ; Aufgabe 2
@@ -30,22 +28,21 @@
 (define (expandiere sym-liste)
   (define (expandiere-iter liste result)
     (let ((c_list (if (and (pair? liste) (number? (car liste))) liste (append (list 1) liste))))
-    (if (null? c_list) 
-        result        
-          (if (> (length c_list) 1)
+      (if (> (length c_list) 1)
               (expandiere-iter (cddr c_list)
                                (append result
                                        (expandiere-helper (car (cdr c_list)) (car c_list) '())))
-              result))))
+              result)
 
-  (expandiere-iter sym-liste '()))
+      ))(expandiere-iter sym-liste '()))
+
+  
 
 
 ; Aufgabe 3
 
 (define (loeschen liste n)
   (if (> n 0) (loeschen (cdr liste) (- n 1)) liste))
-
 
 
 
